b7c9888777adb6b0015ad0940d9a8d2181257702
peterzhang <zhh520@gmail.com>
2018-01-28 17:21:46 +0800

    Update whitelist of url

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,7 +1,8 @@
 'use strict';
 
 export default (req, res, next) => {
-  if (req.path != "/login" && (typeof req.session === 'undefined' || typeof req.session.user === 'undefined')) {
+  const whitelist = ["/login", "/"];
+  if (!whitelist.includes(req.path) && (typeof req.session === 'undefined' || typeof req.session.user === 'undefined')) {
     let err = new Error("Access denied");
     err.status = 403;
     next(err);
